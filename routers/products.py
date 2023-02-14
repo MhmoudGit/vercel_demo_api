6f59a0c001342acd011c672f1f92bae6267cf66a
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-02-14 15:24:20 +0200

    update

--- routers/products.py
+++ routers/products.py
@@ -27,9 +27,9 @@ def get_products():
 # post/create product to products path
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def create_product(product: Product):
-    if product.price_kg is None and product.price_item is None:
+    if (product.price_kg is None and product.price_item is None) or (product.price_kg and product.price_item):
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Either price_kg or price_item must be specified")
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Either price_kg or price_item must be specified and not both")
     product_dict = product.dict()
     product_dict["id"] = randrange(0, 10000000)
     my_products.append(product_dict)
@@ -55,3 +55,17 @@ def delete_product(id: int):
     else: 
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {id} doesnt exist")
+
+# updating a product
+@router.put('/{id}')
+def update_product(id: int, product: Product):
+    p = find_product(id)
+    product_dict = product.dict()
+    if p is not None and p['id'] == id:
+        product_dict['id'] = id
+        my_products[my_products.index(p)] = product_dict
+    else: 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {id} doesnt exist")
+    print(p)
+    return {'data' : product}
