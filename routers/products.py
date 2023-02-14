ae47de22295eaae35b6cc540a89163d466f8c69c
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-02-14 13:55:58 +0200

    delete

--- routers/products.py
+++ routers/products.py
@@ -1,11 +1,11 @@
 # import apirouter to be able to use this file in main.py
-from fastapi import APIRouter, HTTPException,status
+from fastapi import APIRouter, HTTPException, status
 # importing models for schemas
 from models.ProductModel import Product
 from random import randrange
 from data.db import my_products
 
-#------needed logic -------#
+# ------needed logic -------#
 # find poroduct by id
 def find_product(id: int):
     for p in my_products:
@@ -15,21 +15,21 @@ def find_product(id: int):
 
 # create an instance of apirouter and call it
 router = APIRouter(
-    prefix="/products", 
+    prefix="/products",
     tags=["products"],
 )
 
 # get all products path
-@router.get('/')  #its jus '/' because the prefix is set to products so no need to write '/products'
+@router.get('/')    # its just '/' because the prefix is set to products so no need to write '/products'
 def get_products():
     return {'data': my_products}
 
-# post product to products path
-@router.post('/')
+# post/create product to products path
+@router.post('/', status_code=status.HTTP_201_CREATED)
 def create_product(product: Product):
     if product.price_kg is None and product.price_item is None:
         raise HTTPException(
-            status_code=400, detail="Either price_kg or price_item must be specified")
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Either price_kg or price_item must be specified")
     product_dict = product.dict()
     product_dict["id"] = randrange(0, 10000000)
     my_products.append(product_dict)
@@ -43,4 +43,15 @@ def get_product(id: int):
         return p
     else:
         raise HTTPException(
-            status_code= status.HTTP_404_NOT_FOUND, detail=f"Product with id {id} doesnt exist")
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {id} doesnt exist")
+
+#delete a single product
+@router.delete('/{id}')
+def delete_product(id: int):
+    p = find_product(id)
+    if p:
+        my_products.remove(p)
+        return p
+    else: 
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Product with id {id} doesnt exist")
