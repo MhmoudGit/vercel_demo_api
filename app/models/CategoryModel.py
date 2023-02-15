4cec17c4e2fd6c9b1a74a473c16531d403c1b019
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-02-15 14:24:58 +0200

    categorymodel

--- app/models/CategoryModel.py
+++ app/models/CategoryModel.py
@@ -1,7 +1,9 @@
 # import base from db file for the schemas
 from ..data.db import Base
 from sqlalchemy import Column, Integer, String, TIMESTAMP, text
+from sqlalchemy.orm import relationship
 from pydantic import BaseModel
+from .ProductModel import ProductCreate
 
 # postgres model for product of the postgres database
 class Category(Base):
@@ -10,8 +12,10 @@ class Category(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     category_name = Column(String, nullable=False, unique=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')) 
+    products = relationship("Product")
 
 
 # pydantic model for creating products from api for fastapi
 class CategoryCreate(BaseModel):
-    category_name: str
+    category_name: str 
+    products: ProductCreate
