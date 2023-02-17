f4de4cdf2ce3a28ff0a0a9a658c9fdfe73df2193
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-02-17 03:30:05 +0200

    env

---
+++ app/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+# env configs:
+load_dotenv()
+
+class Settings(BaseSettings):
+    db_username: str
+    db_password: str
+    db_hostname: str
+    db_port: str
+    db_name: str
+
+
+
+settings =  Settings()
--- app/data/db.py
+++ app/data/db.py
@@ -1,19 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
+from ..config import settings
 
-load_dotenv()
 
-#'postgresql://<username>:<password>@<ip-adress/hostname>/<database_name>'
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+# 'postgresql://<username>:<password>@<ip-adress/hostname>:portnumber/<database_name>'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.db_username}:{settings.db_password}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
