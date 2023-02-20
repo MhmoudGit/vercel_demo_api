429209d49881fca9d948cd57a7d62aad38a688ae
MhmoudGit <mahmoud.a.a.y18@gmail.com>
2023-02-20 23:37:02 +0200

    starting

--- run.py
+++ run.py
@@ -2,4 +2,15 @@ import uvicorn
 from app.main import app
 import os
 
-uvicorn.run(app, host="localhost", port=int(os.environ.get("PORT", 8000)))
+@app.on_event("startup")
+async def startup():
+    # Add startup code here
+    print("Starting up...")
+
+@app.on_event("shutdown")
+async def shutdown():
+    # Add shutdown code here
+    print("Shutting down...")
+    
+uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
